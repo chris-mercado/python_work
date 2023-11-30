d12b5ff18a00d91a4a3a4e78901e997712e45c5e
chris-mercado <dev.c.mercado@gmail.com>
2023-11-30 11:15:39 -0500

    added a "string" myValue type

--- print and data type.py
+++ print and data type.py
@@ -15,3 +15,7 @@ myValue=True
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue="Leader"
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
